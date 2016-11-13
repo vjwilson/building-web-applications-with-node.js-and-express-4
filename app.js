4257ad97876fcf047a0ca5c906a93d4d25689ef8
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2016-11-13 06:55:26 -0500

    Add Jade templates

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('views', './src/views');
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
-  res.send('<html><h1>Hello World</h1></html>');
+  res.render('index', { list: ['1', '2', '3'] });
 });
 
 app.get('/books', function(req, res) {
